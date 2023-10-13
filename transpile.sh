afb98595c10682568cc9fe6a3ae9f888239d5569
antoinemvp <147729193+antoinemvp@users.noreply.github.com>
2023-10-13 14:14:58 +0200

    Create transpile.sh

---
+++ transpile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Veuillez fournir le nom du serveur comme argument."
+    exit 1
+fi
+
+sed -e "s/HOSTNAME/$1/" flatcar-ignition/cl.yaml | docker run --rm -i quay.io/coreos/butane:latest
