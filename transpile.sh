38daaaac8ef0b10f1e9036ae2aa3e0baff4225d6
Olivier Boudry <79530946+oboudry-mvp@users.noreply.github.com>
2023-10-20 14:47:13 +0200

    Update transpile.sh update IP address part 2

--- transpile.sh
+++ transpile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Veuillez fournir le nom du serveur comme argument."
-    echo 'example: $> bash transpile.sh flatcar01 > ignition.json'
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Veuillez fournir le nom du serveur et l'IP comme arguments."
+    echo 'example: $> bash transpile.sh flatcar01 192.168.77.101 > ignition.json'
     exit 1
 fi
 
-sed -e "s/HOSTNAME/$1/" $(dirname "$0")/cl.yaml | docker run --rm -i quay.io/coreos/butane:latest
+sed -e "s/HOSTNAME/$1/" -e "s/IPADDRESS/$2" $(dirname "$0")/cl.yaml | docker run --rm -i quay.io/coreos/butane:latest
